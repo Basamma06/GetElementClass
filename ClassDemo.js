70ea7b4b8546c49c4b59346d5fee97b63c118853
Basamma06 <130728940+Basamma06@users.noreply.github.com>
2023-04-15 16:29:49 +0530

    Update ClassDemo.js

--- ClassDemo.js
+++ ClassDemo.js
@@ -1,22 +1,14 @@
-//var items =document.getElementsByClassName('list-group-item');
-//console.log(items);
-//console.log(items[1]);
-// items[1].textContent = 'Hello 2';
-// items[1].style.fontweight = 'bold';
- //items[1].style.backgroundcolor = 'yellow';
+var items =document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+ items[1].textContent = 'Hello 2';
+ items[1].style.fontweight = 'bold';
+ items[1].style.backgroundcolor = 'yellow';
 
- // items.style.backgroundcolor='#f4f4f4';
-// for(var i=0;i<items.length;i++){
-   // items[i].style.backgroundcolor ='#f4f4f4';
-// } 
- console.log(document.domain);
- console.log(document.URL);
- console.log(document.title);
- console.log(document.doctype);
- console.log(document.head);
- console.log(document.body);
- console.log(document.all);
- console.log(document.all[10]);
+  items.style.backgroundcolor='#f4f4f4';
+for(var i=0;i<items.length;i++){
+    items[i].style.backgroundcolor ='#f4f4f4';
+ } 
  
  
  
